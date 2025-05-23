11f65ca6ec7824bbc240e52a19405a104d2ea38f
Serhrd <azhimanavs@gmail.com>
2025-05-23 18:14:26 +0500

    version 1

--- src/main/java/com/example/userservice/model/User.java
+++ src/main/java/com/example/userservice/model/User.java
@@ -10,30 +10,30 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-    @Table(name = "users")
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    public class User {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @Column(nullable = false)
-        private String name;
+    @Column(nullable = false)
+    private String name;
 
-        @Column(nullable = false, unique = true)
-        private String email;
+    @Column(nullable = false, unique = true)
+    private String email;
 
-        @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-        private Set<Subscription> subscriptions = new HashSet<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Subscription> subscriptions = new HashSet<>();
 
-        @CreationTimestamp
-        @Column(updatable = false)
-        private LocalDateTime createdAt;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 
-        @UpdateTimestamp
-        private LocalDateTime updatedAt;
-    }
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
